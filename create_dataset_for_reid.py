17a5ec916d2ba990023c50e870eacd387cdd2533
myeongjun-imlab <myeongjun@kakao.com>
2019-12-20 20:59:18 +0900

    init

---
+++ create_dataset_for_reid.py
@@ -0,0 +1,116 @@
+from conditional_gan import make_generator
+import config
+from pose_dataset import PoseHMDataset
+
+import numpy as np
+
+from tqdm import tqdm
+from skimage.io import imsave
+import os
+
+import pandas as pd
+import pose_transform
+import pose_utils
+from itertools import permutations
+from shutil import copy
+from collections import defaultdict
+
+def filter_not_valid(df_keypoints):
+    def check_valid(x):
+        kp_array = pose_utils.load_pose_cords_from_strings(x['keypoints_y'], x['keypoints_x'])
+        distractor = x['name'].startswith('-1') or x['name'].startswith('0000')
+        return pose_transform.check_valid(kp_array) and not distractor
+    return df_keypoints[df_keypoints.apply(check_valid, axis=1)].copy()
+
+
+def make_pairs(df, pairs_for_each=10):
+    fr, to = [], []
+    for image_name in df['name']:
+        fr_names = [image_name] * pairs_for_each
+        to_names = df['name'].sample(n=pairs_for_each)
+        fr += list(fr_names)
+        to += list(to_names)
+    pair_df = pd.DataFrame(index=range(len(fr)))
+    pair_df['from'] = fr
+    pair_df['to'] = to
+    return pair_df
+
+
+def generate_images(dataset, generator, use_input_pose, out_dir, store_train_images):
+    number = 0
+
+    def deprocess_image(img):
+        return (255 * ((img + 1) / 2.0)).astype(np.uint8)
+
+    if not os.path.exists(out_dir):
+        os.makedirs(out_dir)
+
+    for _ in tqdm(range(dataset._pairs_file_test.shape[0])):
+        number += 1
+        batch, name = dataset.next_generator_sample_test(with_names=True)
+        out = generator.predict(batch)
+        #from_image = deprocess_image(batch[0])
+        out_index = 2 if use_input_pose else 1
+        #to_image = deprocess_image(batch[out_index])
+        generated_image = deprocess_image(out[out_index])
+        out = np.squeeze(generated_image)# np.concatenate([from_image, to_image, generated_image], axis=1))
+        name = name.iloc[0]['from'].replace('.jpg', 'g' + str(number) + '.jpg')
+        imsave(os.path.join(out_dir, name), out)
+
+    if store_train_images:
+       for name in tqdm(os.listdir(dataset._images_dir_train)):
+           copy(os.path.join(dataset._images_dir_train, name), out_dir)
+
+def create_train_file(generated_images_folder, train_file_name, generated_as_separate):
+    train_f = open(train_file_name, 'w')
+    cls_to_num = {}
+    for name in os.listdir(generated_images_folder):
+        cls = name.split('_')[0]
+        cls = int(cls)
+        if cls not in cls_to_num:        
+            cls_to_num[cls] = len(cls_to_num)
+#    print cls_to_num
+    for name in os.listdir(generated_images_folder):
+        attr = name.replace('.', '_').split('_')
+        cls, index = attr[0], attr[-2]
+        cls = int(cls)
+        if generated_as_separate and ('g' in index):
+            num = cls_to_num[cls] + len(cls_to_num)
+        else:
+            num = cls_to_num[cls]
+        print >>train_f, "%s %s" % ('dataset/bounding_box_train/' + name, num)
+   
+    train_f.close()
+
+def test():
+    args = config.args()
+
+    args.images_dir_test = args.images_dir_train
+    args.pairs_file_test = 'data/market-re-id-pairs.csv'
+    pairs_for_each = 2
+    train_file_name = 'train.txt'
+    store_train_images = True
+    generated_as_separate = False
+    
+    df_keypoints = pd.read_csv(args.annotations_file_train, sep=':')
+    df = filter_not_valid(df_keypoints)
+
+    print ('Compute pair for train re-id...')
+    pairs_df_train = make_pairs(df, pairs_for_each)
+    print ('Number of pairs: %s' % len(pairs_df_train))
+    pairs_df_train.to_csv('data/market-re-id-pairs.csv', index=False)
+    
+
+    dataset = PoseHMDataset(test_phase=True, **vars(args))
+    generator = make_generator(args.image_size, args.use_input_pose, args.warp_skip, args.disc_type,
+                               args.warp_agg, args.use_bg, args.pose_rep_type)
+    assert (args.generator_checkpoint is not None)
+    generator.load_weights(args.generator_checkpoint)
+
+    print ("Generate images...")
+    generate_images(dataset, generator, args.use_input_pose, args.generated_images_dir, store_train_images=store_train_images)
+    
+    print ("Creating train file...")
+    create_train_file(args.generated_images_dir, train_file_name, generated_as_separate)
+
+test()
---
+++ demo.py
@@ -0,0 +1,110 @@
+from compute_coordinates import cordinates_from_image_file
+from create_pairs_dataset import filter_not_valid
+import config
+import os
+from shutil import copy, rmtree
+import pandas as pd
+from pose_dataset import PoseHMDataset
+from conditional_gan import make_generator
+from tqdm import tqdm
+from test import generate_images, save_images
+from keras.models import load_model
+from skimage.io import imread, imsave
+from skimage.transform import resize
+import numpy as np
+
+if __name__ == "__main__":
+    args = config.args()
+
+    dataset = 'tmp'
+
+    target_images_folder = 'data/target-images'
+    source_image = 'data/source-image.jpg'
+    bg_image = 'data/bg-image.jpg'
+
+    #For target images use kp from previous frame if not detected
+    interpolate = True
+
+    args.images_dir_test = 'data/' + dataset + '-dataset/test'
+    args.annotations_file_test = 'data/' + dataset + '-annotation-test.csv'
+    args.pairs_file_test = 'data/' + dataset + '-pairs-test.csv'
+    args.bg_images_dir_test = 'data/' + dataset + '-dataset/test-bg'
+
+    args.images_dir_train = 'data/' + dataset + '-dataset/train'
+    args.annotations_file_train = 'data/' + dataset + '-annotation-train.csv'
+    args.pairs_file_train = 'data/' + dataset + '-pairs-train.csv'
+    args.bg_images_dir_train = 'data/' + dataset + '-dataset/train-bg'
+
+    f = open(args.annotations_file_train, 'w')
+    print >>f, 'name:keypoints_y:keypoints_x'
+    f.close()
+
+    f = open(args.pairs_file_train, 'w')
+    print >>f, 'from,to'
+    f.close()
+
+    print ("Annotate image keypoints...")
+    if os.path.exists(args.images_dir_test):
+        rmtree(args.images_dir_test)
+    os.makedirs(args.images_dir_test)
+
+    kp_model = load_model(args.pose_estimator)
+    images_to_annotate = [os.path.join(target_images_folder, name) for name in os.listdir(target_images_folder)]
+    images_to_annotate.sort()
+    images_to_annotate.append(source_image)
+
+    result_file = open(args.annotations_file_test, 'w')
+    print >>result_file, 'name:keypoints_y:keypoints_x'
+    prev_pose_cord = -np.ones((18, 2))
+    for i, image_name in tqdm(enumerate(images_to_annotate)):
+        img = imread(image_name)
+        imsave(os.path.join(args.images_dir_test, os.path.basename(image_name)), resize(img, (128, 64)))
+        pose_cords = cordinates_from_image_file(os.path.join(args.images_dir_test, os.path.basename(image_name)),
+                                                    kp_model)
+
+        if interpolate and image_name != source_image:
+            pose_cords[pose_cords == -1] = prev_pose_cord[pose_cords == -1]
+            prev_pose_cord = pose_cords
+
+
+        print >> result_file, "%s: %s: %s" % (os.path.basename(image_name),
+                                              str(list(pose_cords[:, 0])), str(list(pose_cords[:, 1])))
+        result_file.flush()
+    result_file.close()
+
+    print ("Create pairs dataset...")
+    df_keypoints = pd.read_csv(args.annotations_file_test, sep=':')
+    df = filter_not_valid(df_keypoints)
+    fr_list, to_list = [], []
+    valid_names = set(df['name'])
+    for img_to in os.listdir(target_images_folder):
+        if img_to in valid_names:
+            fr_list.append(os.path.basename(source_image))
+            to_list.append(img_to)
+    pair_df = pd.DataFrame(index=range(len(fr_list)))
+    pair_df['from'] = fr_list
+    pair_df['to'] = to_list
+
+    print ('Number of pairs: %s' % len(pair_df))
+    pair_df.to_csv(args.pairs_file_test, index=False)
+
+    print ("Create bg images...")
+    if os.path.exists(args.bg_images_dir_test):
+        rmtree(args.bg_images_dir_test)
+    os.makedirs(args.bg_images_dir_test)
+
+    for img_to in os.listdir(target_images_folder):
+        if img_to in valid_names:
+            copy(bg_image, os.path.join(args.bg_images_dir_test, img_to.replace('.jpg', '_BG.jpg')))
+
+    print ("Generating images...")
+    dataset = PoseHMDataset(test_phase=True, **vars(args))
+    generator = make_generator(args.image_size, args.use_input_pose, args.warp_skip, args.disc_type,
+                               args.warp_agg, args.use_bg, args.pose_rep_type)
+    assert (args.generator_checkpoint is not None)
+    generator.load_weights(args.generator_checkpoint)
+
+    print ("Generate images...")
+    input_images, target_images, generated_images, names = generate_images(dataset, generator, args.use_input_pose)
+    print ("Save images to %s..." % (args.generated_images_dir, ))
+    save_images(input_images, target_images, generated_images, names, args.generated_images_dir)
